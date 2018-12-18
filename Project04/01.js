e5c75af98a0a41bf1cfac34dcc998606ff77df2d
fantasy150592 <pdphuongit@gmail.com>
2018-12-18 22:17:24 +0700

    change project 01-sign-up

---
+++ Project04/01.js
@@ -0,0 +1,15 @@
+var AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79
+};
+
+
+
+// change code below this line
+let { tomorrow: tempOfTomorrow } = AVG_TEMPERATURES; // change this line
+// change code above this line
+return tempOfTomorrow;
+
+
+console.log(
+    tempOfTomorrow); // should be 79
