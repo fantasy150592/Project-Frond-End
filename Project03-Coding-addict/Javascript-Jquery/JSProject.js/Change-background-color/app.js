dc0f4b5b4c55db224dce4263e6d4f318cd0eddef
fantasy150592 <pdphuongit@gmail.com>
2019-01-06 23:20:10 +0700

    bootstrap and javascript

--- Project03-Coding-addict/Javascript-Jquery/JSProject.js/Change-background-color/app.js
+++ Project03-Coding-addict/Javascript-Jquery/JSProject.js/Change-background-color/app.js
@@ -1,13 +1,27 @@
-const colors = ['yellow', 'green', 'blue', '#f5ed12b', '#f37748', '#067bc2'];
+(function() {
 
-const btn = document.getElementById('btn');
 
-// add event listener
-btn.addEventListener('click', function() {
-    let random = Math.floor(Math.random() * colors.length);
-    const body = document.body;
-    body.style.backgroundColor = colors[random];
+    const hexValue = document.getElementById('hex-value');
+    const btn = document.getElementById('btn');
 
-    console.log(random);
+    // add event listener
+    btn.addEventListener('click', createHex);
 
-});
+    function createHex() {
+        const hexValues = [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'E', 'F'
+        ];
+        let hexColor = '#';
+        const body = document.body;
+        for (let i = 0; i < 6; i++) {
+            let random = Math.floor(Math.random() * hexValues.length);
+            hexColor += hexValues[random];
+        }
+        body.style.backgroundColor = hexColor;
+        hexValue.textContent = hexColor;
+
+        //console.log(random);
+
+    };
+
+})();
