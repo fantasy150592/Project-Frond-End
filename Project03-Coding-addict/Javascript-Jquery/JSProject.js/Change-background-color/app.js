1c8083caf5822ddcedaa466a55fdedf5753a2c57
fantasy150592 <pdphuongit@gmail.com>
2019-01-07 22:41:01 +0700

    change javascript random, pass word

--- Project03-Coding-addict/Javascript-Jquery/JSProject.js/Change-background-color/app.js
+++ Project03-Coding-addict/Javascript-Jquery/JSProject.js/Change-background-color/app.js
@@ -12,16 +12,67 @@
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'E', 'F'
         ];
         let hexColor = '#';
-        const body = document.body;
         for (let i = 0; i < 6; i++) {
-            let random = Math.floor(Math.random() * hexValues.length);
+            const random = Math.floor(Math.random() * hexValues.length);
             hexColor += hexValues[random];
         }
-        body.style.backgroundColor = hexColor;
+        document.body.style.backgroundColor = hexColor;
         hexValue.textContent = hexColor;
 
         //console.log(random);
 
     };
+})();
+//Quote random
 
+(function() {
+    const quotes = [{
+            quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur commodi quae ipsam fugit obcaecati optio?",
+            author: "Lorem"
+        },
+
+        {
+            quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, cumque! Cumque, vero hic quia earum nostrum recusandae alias voluptatibus blanditiis.",
+            author: "consectetur"
+        },
+
+
+    ];
+
+    const btn = document.getElementById('generate-btn');
+    btn.addEventListener('click', function() {
+
+        let random = Math.floor(Math.random() * quotes.length);
+
+        document.getElementById('quote').textContent = quotes[random].quote;
+        document.querySelector('.author').textContent = quotes[random].author;
+
+    });
+})();
+
+//Pass the message
+
+(function() {
+    const messageForm = document.getElementById('message-form');
+    messageForm.addEventListener('submit', function(e) {
+        //prevent default action
+        e.preventDefault();
+        //getting a value
+        const message = document.getElementById('message');
+        const value = message.value;
+        //check the empty value
+        if (value === "") {
+
+            const feedback = document.querySelector('.feedback');
+            feedback.classList.add('show');
+            setTimeout(function() {
+                feedback.classList.remove('show');
+            }, 2000);
+
+        }
+        //change value
+        document.querySelector('.message-content').textContent = value;
+        message.value = "";
+
+    });
 })();
