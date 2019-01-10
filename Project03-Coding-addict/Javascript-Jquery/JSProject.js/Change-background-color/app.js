fe0010d2251fa7ea08267345cbec2d7a4bd08b20
fantasy150592 <pdphuongit@gmail.com>
2019-01-10 16:48:02 +0700

    change

--- Project03-Coding-addict/Javascript-Jquery/JSProject.js/Change-background-color/app.js
+++ Project03-Coding-addict/Javascript-Jquery/JSProject.js/Change-background-color/app.js
@@ -76,3 +76,42 @@
 
     });
 })();
+
+
+// counter Project
+(function() {
+    let counterValue = 0;
+
+    const buttons = document.querySelectorAll('.counterBtn');
+    const counter = document.getElementById('counter');
+
+    buttons.forEach(function(btn) {
+        btn.addEventListener('click', function(event) {
+            //element that we click
+            const value = event.target;
+            //console.log(value);
+
+            if (value.classList.contains('prevBtn')) {
+                counterValue--;
+            } else
+            if (value.classList.contains('nextBtn')) {
+                counterValue++;
+            }
+            counter.textContent = counterValue;
+            //change 
+            if (counterValue === 0) {
+                counter.style.color = "#333333";
+
+            } else
+            if (counterValue < 0) {
+                counter.style.color = "#F6511D";
+            } else
+            if (counterValue > 0) {
+                counter.style.color = "#7fb800";
+            }
+
+
+
+        });
+    });
+})();
